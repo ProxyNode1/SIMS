3e1e458a9691dfdc6d14bc538fbdb0aa655e939f
Vikramaditya Singh Thakur <singh.vikramsingh.vikram92@gmail.com>
2020-04-03 10:29:37 +0530

    renamed from editing3

---
+++ src/sims/OtherInfoData.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sims;
+
+/**
+ *
+ * @author Vic's
+ */
+public class OtherInfoData 
+{
+    private String personMailVal = null, universityMailVal = null, dadContactVal = null, famContactVal = null, addInfoVal = null;
+    
+    static volatile OtherInfoData instance = null;
+
+    
+    OtherInfoData()
+    {}
+    
+    
+    public static OtherInfoData getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new OtherInfoData();
+        }
+        
+        return instance;
+    }
+    
+    
+    public String getPersonMailVal() 
+    {
+        return personMailVal;
+    }
+
+    public void setPersonMailVal(String personMailVal) 
+    {
+        this.personMailVal = personMailVal;
+        System.out.println(personMailVal);
+    }
+
+    
+    public String getUniversityMailVal() 
+    {
+        return universityMailVal;
+    }
+
+    public void setUniversityMailVal(String universityMailVal) 
+    {
+        this.universityMailVal = universityMailVal;
+        System.out.println(universityMailVal);
+    }
+
+    
+    public String getDadContactVal() 
+    {
+        return dadContactVal;
+    }
+
+    public void setDadContactVal(String dadContactVal) 
+    {
+        this.dadContactVal = dadContactVal;
+        System.out.println(dadContactVal);
+    }
+
+    
+    public String getFamContactVal() 
+    {
+        return famContactVal;
+    }
+
+    public void setFamContactVal(String famContactVal) 
+    {
+        this.famContactVal = famContactVal;
+        System.out.println(famContactVal);
+    }
+
+    
+    public String getAddInfoVal() 
+    {
+        return addInfoVal;
+    }
+
+    public void setAddInfoVal(String addInfoVal) 
+    {
+        this.addInfoVal = addInfoVal;
+        System.out.println(addInfoVal);        
+    }   
+}

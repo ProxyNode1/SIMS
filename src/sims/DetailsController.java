06b912b21e51cc4ba9aaea06f18fea55ea428325
Vikramaditya Singh Thakur <singh.vikramsingh.vikram92@gmail.com>
2020-05-09 11:44:55 +0530

    completed save functionality.

--- src/sims/DetailsController.java
+++ src/sims/DetailsController.java
@@ -11,18 +11,209 @@ import javafx.fxml.Initializable;
 
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
+import java.util.regex.Pattern;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Tab;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Vic's
- */
+import javafx.scene.control.Tooltip;
+
+
+
 public class DetailsController implements Initializable 
 {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        //nameField.setTooltip(new Tooltip ("use Fullname"));
+                
+        String tmp1 = oBasicInfoData.getNameVal();
+        if(tmp1 != null)
+        {
+            nameField.setText(tmp1);
+        }
+                 
+        InitilizeTabs();
+    }   
+    
+        
+    void InitilizeTabs()
+    {
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("BasicInfo.fxml"));
+            
+            basicInfoTab.setContent(loader.load());
+            oBasicInfoController = loader.getController();
+        } 
+        catch (Exception ex) 
+        {
+            System.out.println("Cant load 1st tab");
+            System.err.println(ex.getMessage());
+        }
+        
+        
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("SSCInfo.fxml"));
+            
+            sscInfoTab.setContent(loader.load());
+            oSSCInfoController = loader.getController();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Cant load 2nd tab");
+            System.err.println(ex.getMessage());
+        }
+        
+        
+        try 
+        {   
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("HSSInfo.fxml"));
+            
+            hssInfoTab.setContent(loader.load());
+            oHSSInfoController = loader.getController();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Cant load 3rd tab");
+            System.err.println(ex.getMessage());
+        }
+        
+        
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("DiplomaInfo.fxml"));
+            
+            diplomaInfoTab.setContent(loader.load());
+            oDiplomaInfoController = loader.getController();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Cant load 4th tab");
+            System.err.println(ex.getMessage());
+        }
+        
+        
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("UGInfo.fxml"));
+            
+            ugInfoTab.setContent(loader.load());
+            oUGInfoController = loader.getController();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Cant load 5th tab");
+            System.err.println(ex.getMessage());
+        }
+        
+        
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PGInfo.fxml"));
+            
+            pgInfoTab.setContent(loader.load());
+            oPGInfoController = loader.getController();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Cant load 6th tab");
+            System.err.println(ex.getMessage());
+        }
+        
+        
+        try 
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("OtherInfo.fxml"));
+            
+            otherInfoTab.setContent(loader.load());
+            oOtherInfoController = loader.getController();
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Cant load 7th tab");
+            System.err.println(ex.getMessage());
+        }
+    }
+    
+            
+    public void getName() throws NullPointerException
+    {       
+        String tmp = nameField.getText();
+        if(tmp == null || !Pattern.compile("[a-zA-Z]+", Pattern.CASE_INSENSITIVE).matcher(tmp).matches())
+        { 
+            nameField.setText(null);
+            oBasicInfoData.setNameVal(null);
+            nameField.setStyle(errorStyle);
+            
+            throw new NullPointerException("name null");
+        } 
+        
+        else 
+        { 
+            nameField.setText(tmp);
+            nameField.setStyle(null); 
+            oBasicInfoData.setNameVal(tmp);            
+        }
+    }
+     
+    
+    public void validateData()
+    {
+        getName();
+        oBasicInfoController.ValidateFields();
+        oSSCInfoController.ValidateFields();
+        oHSSInfoController.ValidateFields();
+        oDiplomaInfoController.ValidateFields();
+        oUGInfoController.ValidateFields();
+        oPGInfoController.ValidateFields();
+        oOtherInfoController.ValidateField();
+    }
+    
+    
+    public void saveData()
+    {
+        validateData();
+    }
+    
+    
+    public void updateData()
+    {
+        
+    }
+    
+    
+    public void deleteData()
+    {
+        
+    }
+    
+        
+    public void toHomepg(MouseEvent event) 
+    {
+        try 
+        {
+            Parent editPag1 = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+            Scene editPg1Scene = new Scene(editPag1);
+            Stage appStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            appStage.setScene(editPg1Scene);
+            appStage.show();
+        } 
+        catch (Exception e) 
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    
+    
+    private final String errorStyle = "-fx-border-color: #ff2323;";   
 
     @FXML
     private JFXTextField nameField;
@@ -47,86 +238,17 @@ public class DetailsController implements Initializable
 
     @FXML
     private Tab otherInfoTab;
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) 
-    {
-        
-        nameField.setText("");
-        
-        try 
-        {
-            //Anc1 = loader.load(this.getClass().getResource("Test2"));
-            basicInfoTab.setContent(FXMLLoader.load(getClass().getResource("BasicInfo.fxml")));
-        } 
-        catch (Exception ex) 
-        {
-            System.out.println("Cant load 1st tab");
-            System.err.println(ex.getMessage());
-        }
-        
-        try 
-        {
-            sscInfoTab.setContent(FXMLLoader.load(getClass().getResource("SSCInfo.fxml")));
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("Cant load 2nd tab");
-            System.err.println(ex.getMessage());
-        }
-        
-        try 
-        {
-            hssInfoTab.setContent(FXMLLoader.load(getClass().getResource("")));
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("Cant load 3rd tab");
-            System.err.println(ex.getMessage());
-        }
-        
-        try 
-        {
-            diplomaInfoTab.setContent(FXMLLoader.load(getClass().getResource("")));
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("Cant load 4th tab");
-            System.err.println(ex.getMessage());
-        }
-        
-        try 
-        {
-            ugInfoTab.setContent(FXMLLoader.load(getClass().getResource("")));
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("Cant load 5th tab");
-            System.err.println(ex.getMessage());
-        }
-        
-        try 
-        {
-            pgInfoTab.setContent(FXMLLoader.load(getClass().getResource("")));
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("Cant load 6th tab");
-            System.err.println(ex.getMessage());
-        }
-        
-        try 
-        {
-            otherInfoTab.setContent(FXMLLoader.load(getClass().getResource("")));
-        } 
-        catch (IOException ex) 
-        {
-            System.out.println("Cant load 7th tab");
-            System.err.println(ex.getMessage());
-        }
-    }    
     
+    private final BasicInfoData oBasicInfoData = BasicInfoData.getInstance();
+    
+    
+    private BasicInfoController oBasicInfoController;
+    private SSCInfoController oSSCInfoController;
+    private HSSInfoController oHSSInfoController;
+    private DiplomaInfoController oDiplomaInfoController;
+    private UGInfoController oUGInfoController;
+    private PGInfoController oPGInfoController;
+    private OtherInfoController oOtherInfoController;
 }
 
 
